adcffe36b5df85fcd7e903813336cf60f5538204
Caner <ic20b038technikum-wien.at>
2023-03-08 16:34:32 +0100

    This commit update the Controller

---
+++ src/main/java/at/fhtw/bic/controller/SilController.java
@@ -0,0 +1,14 @@
+package at.fhtw.bic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SilController {
+
+    @GetMapping("/circle/{num}")
+    double calcCirceArea(@PathVariable double num) {
+        return (num * num)* 3.14159265;
+    }
+}
