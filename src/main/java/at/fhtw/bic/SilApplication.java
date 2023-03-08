075cfecc12e8e1a2a7af4ad2404630c12e66a44b
Caner <ic20b038technikum-wien.at>
2023-03-08 15:43:44 +0100

    Initial commit #1

---
+++ src/main/java/at/fhtw/bic/SilApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/bic/SilApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
